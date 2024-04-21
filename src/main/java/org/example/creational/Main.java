6992e96b4f5e34435660ca6d9484335fc0efaf1a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-04-21 09:06:46 +0530

    Started with design patters

---
+++ src/main/java/org/example/creational/Main.java
@@ -0,0 +1,7 @@
+package org.example.creational;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/creational/Singleton.java
@@ -0,0 +1,6 @@
+package org.example.creational;
+
+public class Singleton {
+
+
+}
